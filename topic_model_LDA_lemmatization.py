cb5708b06af0da676816be1764a12ff41b084a43
Md Monsur Hossain <tonmoy2114@gmail.com>
2018-02-20 13:31:35 -0800

    visualization

--- topic_model_LDA_lemmatization.py
+++ topic_model_LDA_lemmatization.py
@@ -1,4 +1,4 @@
-
+#TODO Read https://www.analyticsvidhya.com/blog/2017/01/ultimate-guide-to-understand-implement-natural-language-processing-codes-in-python/
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
 from gensim.corpora import Dictionary
@@ -6,43 +6,53 @@ from stop_words import get_stop_words
 from gensim.models import Phrases
 from pprint import pprint
 from gensim.models import LdaModel
+from gensim.models import HdpModel
 import gensim
 import copy
 import csv
+from datetime import datetime
+import pyLDAvis.gensim as gensimvis
+import pyLDAvis
 
-writeFileTokens = open('output/post_tokens.csv', 'w+')
-writeFileStopWordRemoved = open('output/post_after_stopwords.csv', 'w+')
+writeFileTokens = open('output/post_comment_tokens.csv', 'w+')
+writeFileStopWordRemoved = open('output/post_comment_after_stopwords.csv', 'w+')
 
 tokenizer = RegexpTokenizer(r'\w+')
 
 docs = []
 
+remove_word_list = ['http','https', 'www','ve','er','nt', 'can', 're', 'com']
+
 # create English stop words list
 en_stop = get_stop_words('en')
 
-with open('postdata.csv') as csvLayerFile:
+print("Program Started at: " + str(datetime.now()))
+
+with open('postCommentCombined.csv') as csvLayerFile:
     csvReader = csv.reader(csvLayerFile)
     i = 0
     for row in csvReader:
-        #if i==50:
-            #break
-        i = i+1
-        docs.append(row[2])
+        mStr = ""
+        for item in row:
+            mStr+=item +"\n"
+        docs.append(mStr)
 
 for idx in range(len(docs)):
     docs[idx] = docs[idx].lower()  # Convert to lowercase.
     docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+    #remove word mentioned in remove_word_list
+    docs[idx] = [x for x in docs[idx] if x not in remove_word_list]
+
     writeFileTokens.write(str(docs[idx])[1:-1])
     writeFileTokens.write("\n")
     #print(docs[idx])
     # remove stop words from tokens
-    #docs[idx] = [i for i in docs[idx] if not i in en_stop]
-    #writeFileStopWordRemoved.write(str(docs[idx])[1:-1])
-    #writeFileStopWordRemoved.write("\n")
+    docs[idx] = [i for i in docs[idx] if not i in en_stop]
+    writeFileStopWordRemoved.write(str(docs[idx])[1:-1])
+    writeFileStopWordRemoved.write("\n")
 
 writeFileTokens.close()
 writeFileStopWordRemoved.close()
-
 
 # Remove numbers, but not words that contain numbers.
 docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
@@ -54,20 +64,23 @@ for doc in docs:
     mfile.write(str(doc)[1:-1])
     mfile.write("\n")
 
+
+
 # Lemmatize the documents.
+
 
 # Lemmatize all words in documents.
 lemmatizer = WordNetLemmatizer()
 docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
 
-mfile = open('output/post_after_lemmatization.csv', 'w+')
+mfile = open('output/post_comment_after_lemmatization.csv', 'w+')
 for doc in docs:
     mfile.write(str(doc)[1:-1])
     mfile.write("\n")
 # Compute bigrams.
 
-# Add bigrams and trigrams to docs (only ones that appear 10 times or more).
-bigram = Phrases(docs, min_count=10)
+# Add bigrams and trigrams to docs (only ones that appear 15 times or more).
+bigram = Phrases(docs, min_count=15)
 for idx in range(len(docs)):
     for token in bigram[docs[idx]]:
         if '_' in token:
@@ -75,24 +88,23 @@ for idx in range(len(docs)):
             docs[idx].append(token)
 
 # Remove rare and common tokens.
-mfile = open('output/post_after_bigram.csv', 'w+')
+mfile = open('output/post_comment_after_bigram.csv', 'w+')
 for doc in docs:
     mfile.write(str(doc)[1:-1])
     mfile.write("\n")
-exit(1)
+
 
 # Create a dictionary representation of the documents.
 dictionary = Dictionary(docs)
 
 # Filter out words that occur less than 20 documents, or more than 50% of the documents.
-#dictionary.filter_extremes(no_below=20, no_above=0.5)
+dictionary.filter_extremes(no_below=20, no_above=0.6)
 
 
 # Vectorize data.
 
 # Bag-of-words representation of the documents.
 corpus = [dictionary.doc2bow(doc) for doc in docs]
-
 
 
 print('Number of unique tokens: %d' % len(dictionary))
@@ -104,7 +116,7 @@ print('Number of documents: %d' % len(corpus))
 # Set training parameters.
 num_topics = 10
 chunksize = 2000
-passes = 10
+passes = 3
 iterations = 40
 eval_every = None  # Don't evaluate model perplexity, takes too much time.
 
@@ -112,10 +124,18 @@ eval_every = None  # Don't evaluate model perplexity, takes too much time.
 temp = dictionary[0]  # This is only to "load" the dictionary.
 id2word = dictionary.id2token
 
-model = LdaModel(corpus=corpus, id2word=id2word, chunksize=chunksize, \
+'''
+#model = LdaModel(corpus=corpus, id2word=id2word, chunksize=chunksize, \
                        alpha='auto', eta='auto', \
                        iterations=iterations, num_topics=num_topics, \
                        passes=passes, eval_every=eval_every)
+'''
 
+#model = LdaModel(corpus, num_topics=num_topics, id2word = id2word, passes=passes)
+#model.save('reddit_autism_pass3_topic10_lda.model')
 
-print(model.print_topics(num_topics=10, num_words=10))
+hdp = HdpModel(corpus, dictionary)
+hdp.save('reddit_autism_hdp.model')
+
+print(model.print_topics(num_topics=num_topics, num_words=12))
+print("Program Ended at: " + str(datetime.now()))
